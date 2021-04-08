b22cc44a70efdc877a4e43a9b450412b0f6cf243
Carl Schweitzer <carl@caschw.com>
2021-04-08 13:16:12 -0500

    Init code check in

---
+++ ViperHold/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading;
+
+namespace ViperHold
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                var current = DateTime.Now;
+                Console.Write(current.ToLongTimeString());
+                Console.SetCursorPosition(0, Console.CursorTop);
+
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
